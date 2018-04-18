84a73cb8c727e104ccf1a2a618688960694afedb
chene <chene@wix.com>
2018-04-18 10:20:37 +0300

    added more missing fields

--- index.js
+++ index.js
@@ -71,10 +71,12 @@ app.get('/getLeagueTable', function (req, res) {
             const leageData = [];
             const table = document.querySelector('.playoff-container');
             const teams = table.querySelectorAll('.table_row');
+            const season = "2017-2018";
+
             teams.forEach((team, index) => {
                 const res = {};
 
-                res._id = `0000${index + 1}`;
+                res._id = `${season}-${index + 1}`;
                 const rankSR = team.childNodes[0].children[0].innerText;
                 res.rank = parseInt(team.childNodes[0].innerText.replace(rankSR, ''));
                 const teamNameSR = team.childNodes[1].children[0].innerText;
@@ -109,85 +111,6 @@ app.get('/getLeagueTable', function (req, res) {
     });
 });
 
-// app.get('/getPlayerStatistics/:playerId', function (req, res) {
-
-//     const playerId = req.params.playerId;
-
-//     let scrape = async () => {
-//         const browser = await puppeteer.launch({
-//             args: ['--no-sandbox', '--disable-setuid-sandbox']
-//         });
-//         const page = await browser.newPage();
-
-//         await page.goto(`http://football.org.il/players/player/?player_id=${playerId}`);
-
-//         await page.exposeFunction('getPlayerId', () => playerId);
-
-//         const result = await page.evaluate(async () => {
-//             const data = [];
-//             const table = document.querySelector('.season-games .table_view');
-//             const games = table.querySelectorAll('.table_row');
-//             const playerId2 = await window.getPlayerId();
-//             const season = "2017-2018";
-//             games.forEach((game, index) => {
-//                 const res = {};
-
-//                 const urlParams = game.href.split("=");
-//                 const gameId = urlParams[urlParams.length - 1];
-
-//                 const isKatamonGame = game.childNodes[5].textContent.indexOf('הפועל קטמון ירושלים') > 0;
-//                 const isBogrimGame = game.childNodes[3].textContent.indexOf('נוער') < 0;
-
-//                 if (isKatamonGame && isBogrimGame) {
-//                     res._id = `${playerId2}-${season}-${index}`;
-//                     res.order = index;
-//                     const dateSR = game.childNodes[1].children[0].innerText;
-//                     res.date = game.childNodes[1].innerText.replace(dateSR, '');
-//                     const misgeretSR = game.childNodes[3].children[0].innerText;
-//                     res.misgeret = game.childNodes[3].innerText.replace(misgeretSR, '');
-//                     const nameSR = game.childNodes[5].children[0].innerText;
-//                     res.name = game.childNodes[5].innerText.replace(nameSR, '');
-//                     res.season = season;
-//                     res.gameId = gameId;
-
-//                     const substitutionTimeSR = game.childNodes[9].children[0].innerText;
-//                     const substitutionTime = game.childNodes[9].innerText.replace(substitutionTimeSR, '');
-//                     res.substitutionTime = substitutionTime;
-//                     if (substitutionTime) {
-//                         const substitutionOut = game.childNodes[9].querySelector('.change-down');
-//                         res.substitution = substitutionOut ? 'out' : 'in';
-//                     } else {
-//                         res.substitution = '';
-//                     }
-//                     const resultSR = game.childNodes[11].children[0].innerText;
-//                     res.result = game.childNodes[11].innerText.replace(resultSR, '');
-
-//                     const yellowCard = game.childNodes[13].querySelector('.card-yellow');
-//                     res.yellowCards = yellowCard ? 'Yellow' : '';
-
-//                     const redCards = game.childNodes[15].querySelector('.card-red');
-//                     res.redCards = redCards ? 'Red' : '';
-
-//                     res.playerId = playerId2;
-
-//                     const goalsSR = game.childNodes[17].children[0].innerText;
-//                     res.goals = game.childNodes[17].innerText.replace(goalsSR, '');
-
-
-//                     data.push(res);
-//                 }
-//             });
-//             return data;
-//         });
-
-//         browser.close();
-//         return result;
-//     };
-
-//     scrape().then((value) => {
-//         res.send(JSON.stringify(value));
-//     });
-// });
 
 app.get('/getGamePlayersData/:gameId', function (req, res) {
 
@@ -220,7 +143,8 @@ app.get('/getGamePlayersData/:gameId', function (req, res) {
                 res.order = index;
                 res.shirtNum = player.querySelector('.number').innerText.match(numberPattern)[0];
                 res.playerState = playerStatus;
-                res.gameId = gameId;
+                res.gameId = gameId2;
+                res.playerId = playerId;
                 res.isHome = isHome;
                 res.name = player.querySelector('.name').innerText;
                 res.season = season;
